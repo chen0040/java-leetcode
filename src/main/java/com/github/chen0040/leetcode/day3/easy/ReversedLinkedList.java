9854594a839e72549088a82439d7001bd6596458
xschen <xs0040@gmail.com>
2017-07-29 21:10:10 +0800

    1 easy for today

---
+++ src/main/java/com/github/chen0040/leetcode/day3/easy/ReversedLinkedList.java
@@ -0,0 +1,33 @@
+package com.github.chen0040.leetcode.day3.easy;
+
+
+/**
+ * Created by xschen on 29/7/2017.
+ *
+ * summary:
+ * Reverse a singly linked list.
+ *
+ * link: https://leetcode.com/problems/reverse-linked-list/description/
+ */
+public class ReversedLinkedList {
+ public class ListNode {
+     int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+   public class Solution {
+      public ListNode reverseList(ListNode head) {
+         ListNode x = head;
+         ListNode first = null;
+         while(x != null) {
+            ListNode x_next = x.next;
+            ListNode oldFirst = first;
+            first = x;
+            first.next = oldFirst;
+            x = x_next;
+         }
+         return first;
+      }
+   }
+}
