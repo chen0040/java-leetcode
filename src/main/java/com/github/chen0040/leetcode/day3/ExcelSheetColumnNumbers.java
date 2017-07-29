665919cbc98c0c5b463fd0c8cc16b1b79fb6b380
xschen <xs0040@gmail.com>
2017-07-29 14:39:02 +0800

    day 3 easy 3 solved

---
+++ src/main/java/com/github/chen0040/leetcode/day3/ExcelSheetColumnNumbers.java
@@ -0,0 +1,26 @@
+package com.github.chen0040.leetcode.day3;
+
+
+/**
+ * Created by xschen on 29/7/2017.
+ *
+ * summary:
+ * Related to question Excel Sheet Column Title
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ * 
+ * link: https://leetcode.com/problems/excel-sheet-column-number/description/
+ */
+public class ExcelSheetColumnNumbers {
+   public class Solution {
+      public int titleToNumber(String s) {
+         int start = (int)'A';
+         int result = 0;
+         for(int i = 0; i < s.length(); ++i) {
+            int c = (int)s.charAt(i);
+            c = c - start + 1;
+            result = result * 26 + c;
+         }
+         return result;
+      }
+   }
+}
