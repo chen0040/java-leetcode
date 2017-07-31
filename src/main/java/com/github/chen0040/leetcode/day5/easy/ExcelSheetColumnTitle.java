fd3c8352059faab5e69cb0854427243955abc1ba
xschen <xs0040@gmail.com>
2017-07-31 11:09:11 +0800

    excel sheet column title and linked list cycle

---
+++ src/main/java/com/github/chen0040/leetcode/day5/easy/ExcelSheetColumnTitle.java
@@ -0,0 +1,31 @@
+package com.github.chen0040.leetcode.day5.easy;
+
+
+/**
+ * Created by xschen on 31/7/2017.
+ *
+ * summary:
+ * Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+ *
+ * link: https://leetcode.com/problems/excel-sheet-column-title/description/
+ */
+public class ExcelSheetColumnTitle {
+   public class Solution {
+      public String convertToTitle(int n) {
+         String result = "";
+         int int_A = (int)'A';
+         while(n > 0) {
+            int digit = n % 26;
+            n /= 26;
+            if(digit == 0) {
+               n--;
+               digit = 26;
+            }
+            char c = (char)(int_A + digit - 1);
+            result = c + result;
+         }
+
+         return result;
+      }
+   }
+}
