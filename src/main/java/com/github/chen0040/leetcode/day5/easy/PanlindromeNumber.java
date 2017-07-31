983e8cd86e51f5fea9f1ab1015aed46b86b3a273
xschen <xs0040@gmail.com>
2017-07-31 12:08:41 +0800

    longest common prefix and panlindrome number

---
+++ src/main/java/com/github/chen0040/leetcode/day5/easy/PanlindromeNumber.java
@@ -0,0 +1,31 @@
+package com.github.chen0040.leetcode.day5.easy;
+
+
+/**
+ * Created by xschen on 31/7/2017.
+ *
+ * summary:
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ *
+ * link: https://leetcode.com/problems/palindrome-number/description/
+ */
+public class PanlindromeNumber {
+   public class Solution {
+      private int reverseInt(int x) {
+         int result = 0;
+         while(x > 0) {
+            int digit = x % 10;
+            result += digit;
+            x /= 10;
+            if(x > 0) {
+               result = result * 10;
+            }
+         }
+         return result;
+      }
+      public boolean isPalindrome(int x) {
+         int x2 = reverseInt(x);
+         return x2 == x;
+      }
+   }
+}
